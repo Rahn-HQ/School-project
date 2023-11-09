6a677e77c24504cc56f9583aa1102ee02d843abf
Abd11ellah <abdulilahalhamwi@gmail.com>
2023-11-09 21:45:14 +0300

    uploded the main page

--- components/index.js
+++
@@ -1,7 +0,0 @@
-export {default as Navbar} from './Navbar'
-export {default as Header} from './Header'
-export {default as FirstSec} from './FirstSec'
-export {default as SecondSec} from './SecondSec'
-export {default as ThirdSec} from './ThirdSec'
-export {default as Footer} from './Footer'
-
