67fec74dc92ccc486c5d2e3f86f5f43a53d44207
Abd11ellah <abdulilahalhamwi@gmail.com>
2023-12-01 18:21:41 +0300

    added some animation

--- src/dbconfig/dbconfig.ts
+++ src/dbconfig/dbconfig.ts
@@ -1,20 +1,20 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// const url = process.env.MONGO_URL as string;
-// const connect = async ()=>{
-//     try {
-//         mongoose.connect(url)
-//         const connection = mongoose.connection;
-//         connection.on('connected', () => {
-//             console.log('MongoDB connected successfully!');
-//         })
-//         connection.on('error', (err) => {
-//             console.log('MongoDB connection error. Please make sure MongoDB is running. ' + err);
-//             process.exit();
-//         });
-//     } catch (error) {
-//         console.log('Something went wrong');
-//         console.log(error);
-//     }
-// }
-// export default connect();
+const url = process.env.MONGO_URL as string;
+const connect = async ()=>{
+    try {
+        mongoose.connect(url)
+        const connection = mongoose.connection;
+        connection.on('connected', () => {
+            console.log('MongoDB connected successfully!');
+        })
+        connection.on('error', (err) => {
+            console.log('MongoDB connection error. Please make sure MongoDB is running. ' + err);
+            process.exit();
+        });
+    } catch (error) {
+        console.log('Something went wrong');
+        console.log(error);
+    }
+}
+export default connect();
