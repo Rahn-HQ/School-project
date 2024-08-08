3a30f4c35d4908be7c524154c2614fbc914f120a
Abd11ellah <abdulilahalhamwi@gmail.com>
2024-08-08 18:53:25 +0300

    malek student page

--- src/app/student-profile/page.jsx
+++ src/app/student-profile/page.jsx
@@ -1,18 +1,120 @@
 "use client";
 import React from 'react'
-import { Header} from '../../components'
+import { Header } from '../../components'
+import { info } from '../../utils/studentInfo'
+import { motion } from 'framer-motion';
 
 const Student = () => {
   return (
-    <div>
+    <div className='bg-[url("https://www.transparenttextures.com/patterns/45-degree-fabric-dark.png")]
+    overflow-x-hidden overflow-y-hidden  bg-white   '>
 
-    <div>  
-    
-     <Header img="https://i.postimg.cc/fL9wv380/asdf.jpg" title="SFCA STUDENT PROFILE "/>
-    
-    </div>  
+      <div>
+        <Header img="https://i.postimg.cc/fL9wv380/asdf.jpg" title="SFCA STUDENT PROFILE " />
+      </div>
+      <h1 className=' text-black text-[42px] pl-10 max-md:pl-4 my-10 max-lg:ml-0 max-lg:text-3xl max-md:text-2xl'>
+        MALEK PROFILE</h1>
 
-    
+      <motion.div className='relative flex justify-center
+      pt-5  object-cover h-full w-full
+      pb-10 overflow-x-hidden overflow-y-hidden
+      max-sm:bg-none max-sm:bg-white
+      max-lg:bg-none
+       '
+
+        variants={{
+          hidden: {
+            x: 100,
+            // y: direction === 'up' ? 100 : direction === 'down' ? -100 : 0,
+            opacity: 0,
+          },
+          show: {
+            x: 0,
+            //y: 0,
+            opacity: 1,
+            transition: {
+              type: "spring",
+              delay: 0.2,
+              duration: 1,
+              ease: "easeOut",
+            },
+          },
+        }}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true, amount: 0.25 }}
+      >
+
+
+
+        <div
+          className='w-[80%]  bg-slate-100
+          rounded-lg mb-10
+          max-sm:w-full max-sm:bg-white
+          max-lg:w-full max-lg:bg-white
+           '
+        >
+
+          {/* picture of student  */}
+          <div className='flex justify-center m-auto
+          w-[200px] h-[200px]
+          max-md:w-[150px] max-md:h-[150px] overflow-hidden my-5
+            max-lg:w-[175px] max-lg:h-[150px]
+          '>
+            <picture>
+              <img
+                className="rounded-full "
+                src="https://cdn-icons-png.flaticon.com/512/2784/2784461.png"
+                alt="Student Picture"
+              />
+            </picture>
+          </div>
+
+
+          {/*Student Info  */}
+          <div
+            className='m-5  p-4
+            rounded-3xl  grid grid-cols-2
+             max-sm:grid-cols-1
+            max-sm:w-full max-sm:p-0 max-sm:m-0
+            max-sm:rounded-none
+             '
+          >
+            {info.map((inf, index) => (
+              <div
+                className=' m-1 '
+                key={index}
+              >
+                <div
+                  className='flex justify-start
+                  bg-slate-200 p-2
+                  pl-4 ml-3 my-2 rounded-xl
+                italic text-2xl font-bold
+                max-sm:w-[90%]
+
+                '
+                >
+                  {inf.Lable}
+                </div>
+
+                <div
+                  className='flex justify-start pl-4
+                 w-[90%] ml-3 mb-5 p-3 shadow-xl
+                 rounded-xl bg-slate-100
+                 italic text-2xl font-semibold
+                 max-sm:w-[80%]
+                '
+                >
+
+                  {inf.Content}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+      </motion.div>
+
 
     </div>
   )
---
+++ src/utils/studentInfo.js
@@ -0,0 +1,35 @@
+export const info = [
+  {
+    Lable :"Name",
+    Content : "Malek",
+  },
+  {
+    Lable:"Birthday",
+    Content:"1 / 1 / 2009"
+  },
+  {
+    Lable:"Grade",
+    Content:"7"
+  },
+  {
+    Lable:"Class",
+    Content:2
+  },
+  {
+    Lable:"Father Name",
+    Content :"Rafik"
+  },
+  {
+    Lable:"Mother Name",
+    Content :"Mariam"
+  },
+
+  {
+    Lable:"Adress",
+    Content:"San Fransico"
+  },
+  {
+    Lable:"Phone Number",
+    Content:"+1 987 654 321"
+  }
+]
